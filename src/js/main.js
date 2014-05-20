68e539dcd6e32e3d99da031898d5d22f3c6c2712
Chase Moody <chase.moody@mutualmobile.com>
2014-05-20 14:43:42 -0500

    Fixed almond google maps issue

--- src/js/main.js
+++ src/js/main.js
@@ -6,10 +6,8 @@ require.config({
 	    'hammerjs': 'vendor/jquery-hammerjs/jquery.hammer-full.min',
 	    'velocity': 'vendor/velocity/jquery.velocity.min',
         'text': 'vendor/requirejs-text/text',
-        'async': 'vendor/async',
         'backboneLocalstorage': 'vendor/backbone.localStorage/backbone.localStorage-min',
         'd3': 'vendor/d3/d3.min',
-        'google': 'http://maps.google.com/maps/api/js?key=AIzaSyCJ7xhp7KwIi_rdp8WTVxja2r5KNvA1uK8&sensor=true',
 	    'app': 'app',
         'libs': 'libs'
     },
